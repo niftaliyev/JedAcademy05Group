19f1e53bfd4c974165150ecc9f70e92b35c19588
Kamran <kamran.niftaliyev@eilink.az>
2024-07-10 22:05:34 +0400

    web blog

---
+++ asp.netcore/BlogWebSite/BlogWebSite/Controllers/PostsController.cs
@@ -0,0 +1,43 @@
+﻿using BlogWebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebSite.Controllers;
+
+public class PostsController : Controller
+{
+    private readonly ApplicationDbContext context;
+
+    public PostsController(ApplicationDbContext context)
+    {
+        this.context = context;
+    }
+    public IActionResult Posts()
+    {
+        var posts = context.Posts.ToList();
+        return View(posts);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Post post)
+    {
+        if (string.IsNullOrEmpty(post.Content))
+        {
+            ModelState.AddModelError("Content","hani content ??");
+        }
+        if (ModelState.IsValid)
+        {
+            post.Date = DateTime.Now;
+            context.Add(post);
+            await context.SaveChangesAsync();
+            TempData["Status"] = "New post added!";
+            return RedirectToAction("Posts", "Posts");
+        }
+        return View(post);
+    }
+}
---
+++ asp.netcore/BlogWebSite/BlogWebSite/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogWebSite.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "hani title ??")]
+    [StringLength(100,MinimumLength = 3)]
+    [Display(Name = "Title")]
+    public string Title { get; set; }
+    [Required]
+    public string Content { get; set; }
+    public DateTime Date { get; set; }
+
+    [Display(Name = "Image Url")]
+    public string ImageUrl { get; set; }
+}
